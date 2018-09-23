6328673934317f1c25658ba85bd00009edf8931b
shakirmole <shakirmole@gmail.com>
2018-09-23 23:56:00 +0330

    Email format
    1. Changed email message

--- controllers/remarks.php
+++ controllers/remarks.php
@@ -127,7 +127,6 @@ if ( $action == 'yearly_report_add' ) {
 }
 
 if ( $action == 'yearly_report_save' ) {
-	// die;
 	$id = $_POST['id'];
 
 	$miniData = $_POST['report'];
@@ -142,7 +141,10 @@ if ( $action == 'yearly_report_save' ) {
 		$id = $StudentReports->insert($miniData);
 		$teacher = $Teachers->getDetails($miniData['teacherid']);
 		$enrollment = $Enrollments->getDetails($miniData['enrollid']);
-		$error = sendEmail($teacher['email'], 'HMGS - Student Report Saved', '', "The report for $enrollment[student] has been saved", 'shakirmole@gmail.com', 'HMGS');
+		$body = "The report for $enrollment[student] has been saved <br>
+				 Login to the website then visit <a href='http://shakirmole.epizy.com/qurantracker/?module=remarks&action=yearly_report_edit&id=$id'>this link</a> to review the report";
+
+		$error = sendEmail($teacher['email'], 'HMGS - Student Report Saved', '', $body, 'shakirmole@gmail.com', 'HMGS');
 	} else {
 		$id = $StudentReports->update($id,$miniData);
 	}
